61dd8fe6de600df3176656fc19903a22b1d28819
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2024-02-29 20:38:04 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,22 @@
-import {User} from "../db/user.js";
+import { User } from "../db/user.js";
+import  {registerSchema}  from '../db/user.js'
 
 import {HttpError}  from "../helpers/HttpError.js";
 
 export const registration = async (req, res) => {
 
     const newUser = await User.create(req.body);
-    res.status(201).json({
-        email: newUser.email,
-        password: newUser.password,
-    })
+        try {
+        const validationResult = registerSchema.validate(req.body);
+       if (validationResult.error) {
+            return res.status(400).json({ message: validationResult.error.message });
+        }
+        const newContact = await Contact.create(req.body);
+        res.status(201).json(newContact);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
+}; 
     
-};
