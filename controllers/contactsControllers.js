26b2b3dc24267934e28569b2dadf52f69f0ff50d
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2024-03-13 20:08:56 +0100

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,22 +2,18 @@
 
 
 import { Contact } from '../db/contact.js';
-import { User } from '../db/user.js';
 
 import { createContactSchema } from "../schemas/contactsSchemas.js";
 import {updateContactSchema} from '../schemas/contactsSchemas.js'
 import { updateFavoriteSchema } from '../schemas/contactsSchemas.js'
-import path from "path";
 
-
-import fs from "fs/promises";
 
 export const getAllContacts = async (req, res) => {
   try {
     const { _id: owner } = req.user;
     const { page = 1, limit = 10 } = req.query;
     const skip = (page - 1) * limit;
-        const contacts = await Contact.find({owner}, {skip, limit}).populate("owner", "name email");
+        const contacts = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit,}).populate("owner", "name email");
               res.status(200).json(contacts);
     } catch (error) {
         res.status(500).json({
